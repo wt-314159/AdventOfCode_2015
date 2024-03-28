da85148b0d0d9f762212c4efc5616ca044aba4fb
Will Train <87541128+wt-314159@users.noreply.github.com>
2024-03-28 12:07:33 +0000

    First day completed!

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,27 @@
+use std::{fs, io};
+
 fn main() {
-    println!("Hello, world!");
+    // let mut entries = fs::read_dir(".").expect("Failed to read directory")
+    //     .map(|res| res.map(|e| e.path()))
+    //     .collect::<Result<Vec<_>, io::Error>>().expect("Failed to collect");
+
+    let input = fs::read_to_string("./src/puzzle_input.txt").expect("Failed to read input");
+    println!("{:?}", input);
+
+    let mut floor = 0;
+    let mut negative_index = 0;
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => floor = floor + 1,
+            ')' => floor = floor - 1,
+            other => panic!("Unexpected char: {}", other)
+        }
+        // index of first character to cause count to be -1
+        if (floor == -1 && negative_index == 0) {
+            negative_index = i + 1;
+        }
+    }
+
+    println!("Floor number: {}", floor);
+    println!("Index of negative char {}", negative_index);
 }
