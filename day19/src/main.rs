bb93abdc9488b5735ee3369621b58f7e8cc64768
Will Train <87541128+wt-314159@users.noreply.github.com>
2024-04-05 11:38:21 +0100

    Working on day 19

---
+++ day19/src/main.rs
@@ -0,0 +1,28 @@
+use std::{fs, collections::HashMap};
+
+fn main() {
+    let input = fs::read_to_string("./src/puzzle_input.txt").expect("Failed to read input");
+    //let input = ".#.#.# ...##. #....# ..#... #.#..# ####..";
+    println!("{:?}", input);
+    println!("Input length: {}", input.len());
+
+    let lines: Vec<&str> = input.split("\n").collect();
+    let mut replacements: HashMap<&str, Vec<String>> = HashMap::new();
+    for i in 0..lines.len() - 2 {
+        let line = lines[i];
+        let params: Vec<&str> = line.split_whitespace().collect();
+        let entry = replacements.entry(params[0]).or_insert(Vec::new());
+        entry.push(String::from(params[2]));
+    }
+    
+    let med_molecule = lines.last().unwrap();
+    println!("Parsing complete");
+
+    for replacement in replacements {
+
+    }
+}
+
+fn all_molecules_from_replacement(med_molecue: &str, mol_replacements: (&str, Vec<String>), molecules: &mut Vec<&str>) {
+    
+}
