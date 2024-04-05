71141c3756ec5b1fa6a0961192a5129feb2c9987
Will Train <87541128+wt-314159@users.noreply.github.com>
2024-04-05 13:39:24 +0100

    Testing creating new projects with template

---
+++ day20/src/main.rs
@@ -0,0 +1,16 @@
+#[allow(unused_imports)]
+use std::{fs, collections::HashMap, cmp::min, cmp::max};
+// use fancy_regex::Regex;
+// use regex::Regex;
+
+fn main() {
+    let input = fs::read_to_string("./src/puzzle_input.txt").expect("Failed to read input");
+    //let input = fs::read_to_string("./src/test_puzzle_input.txt").expect("Failed to read test input");
+    println!("{:?}", input);
+    println!("Input length: {}", input.len());
+
+    #[allow(unused_variables)]
+    for line in input.split("\n") {
+
+    }
+}
