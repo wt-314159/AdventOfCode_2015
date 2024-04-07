c40c8bd9f509629a7e744e9ae58f2c889c9d5ba8
Will Train <87541128+wt-314159@users.noreply.github.com>
2024-04-07 23:56:01 +0100

    Finally solving Part 1, though not very efficiently

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -7,12 +7,50 @@ const PRESENTS: u32 = 36000000;
 const TENTH_PRESENTS: u32 = PRESENTS / 10;
 const PRIMES: [u32; 168] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
 
-fn main() {
-    // going to assume it's at least house 128
-    // start with a power of two as powers of two are likely to be highly divisible
-    let mut house_num = 128;
+fn 
+main() {
+    // found a few hits around 900_000 from previous iterations
+    // could also start at a lowish number and double each time, 
+    // and then use narrowing search over the last half
+    // lowest hit yet -> 856800
+    let mut house_num = 856800;
+    house_num = narrowing_search((house_num - 50_000)..house_num, 8192);
 
-    println!("First house to surpass present num is {}", house_num);
+    println!("First house to pass is {}", house_num)
+    //println!("First house to surpass present num is {}", house_num);
+}
+
+// lower the upper limit of a search and hopefully narrow in on the answer
+fn narrowing_search(range: Range<u32>, increment: u32) -> u32 {
+    println!("Searching range {} - {}, increment: {}", range.start, range.end, increment);
+    let mut house_num = range.start;
+    let mut incr = increment;
+    if increment > 3 {
+        incr = increment - 3;
+    }
+    let increments = vec![1,2,incr];
+    let mut incrmnt_index = 0;
+    while house_num <= range.end {
+        if is_enough_presents(house_num) {
+            println!("Upper limit narrowed to {}", house_num);
+            if increment == 1 {
+                return house_num;
+            }
+            return narrowing_search(range.start..house_num, increment / 2);
+        }
+        if increment > 2 {
+            house_num += increments[incrmnt_index];
+            incrmnt_index += 1;
+            if incrmnt_index == increments.len() {
+                incrmnt_index = 0;
+            }
+        }
+        else {
+            house_num += increment;
+        }
+    }
+    println!("Upper limit not narrowed");
+    return narrowing_search(range, increment / 2);
 }
 
 fn binary_search(range: Range<u32>) -> u32 {
@@ -41,7 +79,7 @@ fn is_enough_presents(house_number: u32) -> bool {
     // reduce the number of checks we need to do before
     // returning true (if total number of presents is greater
     // than PRESENTS)
-    for i in (2..=half_house_num).rev() {
+    for i in (2..=half_house_num) {
         if house_number % i == 0 {
             num_presents += i;
         }
@@ -105,39 +143,6 @@ fn factors_from_prime_factors(prime_factors: Vec<u32>) -> Vec<u32> {
     all_factors
 }
 
-// Attempting to reduce the number of modulos we need to take to get a score for a house,
-// by storing factors already worked out and using those later on, turned out much slower
-fn num_presents_for_house_2(house_number: u32, factors: &mut HashMap<u32, (u32, Vec<u32>)>) -> u32 {
-    let half_house_num = house_number / 2;
-    let mut num_presents = 1 + house_number;
-    let mut factor_indices: Vec<u32> = Vec::new();
-
-    for i in (2..=half_house_num).rev() {
-        // num presents already counted
-        let mut already_counted = false;
-        for indices in &factor_indices {
-            if factors[&indices].1.contains(&i) {
-                already_counted = true;
-                break;
-            }
-        }
-        if already_counted {
-            continue;
-        }
-        if house_number % i == 0 {
-            if factors.contains_key(&i) {
-                // add the number of presents that factor got
-                num_presents += factors[&i].0 - 1;
-                factor_indices.push(i);
-                continue;
-            }
-            num_presents += i;
-        }
-    }
-    factors.insert(house_number, (num_presents, factor_indices));
-    num_presents * 10
-}
-
 fn num_presents_for_house(house_number: u32) -> u32 {
     let half_house_num = house_number / 2;
     // Always divisible by 1 and itself
@@ -151,4 +156,3 @@ fn num_presents_for_house(house_number: u32) -> u32 {
     // multiply by 10 at end
     num_presents * 10
 }
-
