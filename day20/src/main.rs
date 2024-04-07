728bdf6e577a7f4e0f4b199f9cb1885f63c00ca2
Will Train <87541128+wt-314159@users.noreply.github.com>
2024-04-07 22:34:04 +0100

    Trying prime factorisation

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,30 +1,18 @@
 #[allow(unused_imports)]
-use std::{fs, collections::HashMap, cmp::min, cmp::max, ops::Range};
+use std::{fs, collections::HashMap, cmp::min, cmp::max, ops::Range, time::Instant};
 // use fancy_regex::Regex;
 // use regex::Regex;
 
 const PRESENTS: u32 = 36000000;
 const TENTH_PRESENTS: u32 = PRESENTS / 10;
+const PRIMES: [u32; 168] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
 
 fn main() {
-    // going to assume it's at least house 100
-    let mut house_num = 100;
-
-    // going by every single house takes too long, try a more efficient search
-    loop {
-        if is_enough_presents(house_num) {
-            let prev_checked_number = house_num / 2;
-            let house_num = binary_search(prev_checked_number..house_num + 1);
-            break;
-        }
-        //println!("Presents: {}", num_presents);
-        house_num = house_num * 2;
-    }
+    // going to assume it's at least house 128
+    // start with a power of two as powers of two are likely to be highly divisible
+    let mut house_num = 128;
 
     println!("First house to surpass present num is {}", house_num);
-    let num_presents = num_presents_for_house(house_num);
-    println!("House {} gets {} presents", house_num, num_presents);
-    println!("House {} gets {} presents", house_num - 1, num_presents_for_house(house_num - 1));
 }
 
 fn binary_search(range: Range<u32>) -> u32 {
@@ -48,7 +36,12 @@ fn is_enough_presents(house_number: u32) -> bool {
     let half_house_num = house_number / 2;
     // always divisible by 1 and itself
     let mut num_presents = 1 + house_number;
-    for i in 2..=half_house_num {
+    // start at half_house_num and work backwards, as this 
+    // will increase the num_presents count quicker and 
+    // reduce the number of checks we need to do before
+    // returning true (if total number of presents is greater
+    // than PRESENTS)
+    for i in (2..=half_house_num).rev() {
         if house_number % i == 0 {
             num_presents += i;
         }
@@ -57,6 +50,92 @@ fn is_enough_presents(house_number: u32) -> bool {
         }
     }
     num_presents >= TENTH_PRESENTS
+}
+
+// turns out prime factorisation is pretty slow, I think because of all
+// the memory allocation we're doing in the implementation, and the checks
+// to ensure we don't duplicate any factors
+// should only have to check if house-number is divisible by prime numbers
+fn presents_from_primes(house_number: u32) -> u32 {
+    // Find all prime factors
+    let prime_factors = find_prime_factors(house_number);
+    // find all factors from prime factors
+    let all_factors = factors_from_prime_factors(prime_factors);
+    // calculate num presents
+    let mut num_presents = all_factors.iter().sum();
+    num_presents *= 10;
+    num_presents
+}
+
+fn find_prime_factors(number: u32) -> Vec<u32> {
+    let mut prime_factors: Vec<u32> = Vec::new();
+    let mut quotient = number;
+
+    while quotient != 1 {
+        for i in 0..PRIMES.len() {
+            let prime = PRIMES[i];
+            if quotient % prime == 0 {
+                prime_factors.push(prime);
+                quotient = quotient / prime;
+                break;
+            }
+        }
+    }
+
+    prime_factors
+}
+
+fn factors_from_prime_factors(prime_factors: Vec<u32>) -> Vec<u32> {
+    // include prime factors
+    let mut all_factors = Vec::new();
+    for i in 0..prime_factors.len() {
+        let mut factor = prime_factors[i];
+        if !all_factors.contains(&factor){
+            all_factors.push(factor);
+        }
+
+        for j in i + 1..prime_factors.len() {
+            factor = factor * prime_factors[j];
+            if !all_factors.contains(&factor){
+                all_factors.push(factor);
+            }
+        }
+    }
+    all_factors.push(1);
+    all_factors
+}
+
+// Attempting to reduce the number of modulos we need to take to get a score for a house,
+// by storing factors already worked out and using those later on, turned out much slower
+fn num_presents_for_house_2(house_number: u32, factors: &mut HashMap<u32, (u32, Vec<u32>)>) -> u32 {
+    let half_house_num = house_number / 2;
+    let mut num_presents = 1 + house_number;
+    let mut factor_indices: Vec<u32> = Vec::new();
+
+    for i in (2..=half_house_num).rev() {
+        // num presents already counted
+        let mut already_counted = false;
+        for indices in &factor_indices {
+            if factors[&indices].1.contains(&i) {
+                already_counted = true;
+                break;
+            }
+        }
+        if already_counted {
+            continue;
+        }
+        if house_number % i == 0 {
+            if factors.contains_key(&i) {
+                // add the number of presents that factor got
+                num_presents += factors[&i].0 - 1;
+                factor_indices.push(i);
+                continue;
+            }
+            num_presents += i;
+        }
+    }
+    factors.insert(house_number, (num_presents, factor_indices));
+    num_presents * 10
 }
 
 fn num_presents_for_house(house_number: u32) -> u32 {
@@ -72,3 +151,4 @@ fn num_presents_for_house(house_number: u32) -> u32 {
     // multiply by 10 at end
     num_presents * 10
 }
+
